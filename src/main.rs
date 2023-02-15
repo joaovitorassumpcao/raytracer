7dcab3d9cc7302067269633ef914d142d976d509
joaovitorassumpcao <joao.assumpo@gmail.com>
2023-02-15 16:59:52 -0300

    generates red 256x256 png

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use image::{ImageBuffer, RgbImage};
+
 fn main() {
-    println!("Hello, world!");
+    let mut img: RgbImage = ImageBuffer::new(256, 256);
+
+    for (_x, _y, pixel) in img.enumerate_pixels_mut() {
+        *pixel = image::Rgb([255,0,0]);
+    }
+
+    img.save("red.png").expect("image error");
 }
