ba17f476c9dac7f7e52aeee2c717064dd5e9854f
joaovitorassumpcao <joao.assumpo@gmail.com>
2023-02-15 18:18:48 -0300

    `task 1.2` gradients

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use image::{ImageBuffer, RgbImage};
 
 fn main() {
+    let (w, h) = (300, 300);
     let mut img: RgbImage = ImageBuffer::new(256, 256);
 
-    for (_x, _y, pixel) in img.enumerate_pixels_mut() {
-        *pixel = image::Rgb([255,0,0]);
+    for (i, j, pixel) in img.enumerate_pixels_mut() {
+        let r = i as f64 / (w - 1) as f64;
+        let g = j as f64 / (h - 1) as f64;
+        let b = 0.25;
+        *pixel = image::Rgb([r,g,b].map(|x| (x * 255.999) as u8));
     }
 
-    img.save("red.png").expect("image error");
+    img.save("gradient.png").expect("image error");
 }
