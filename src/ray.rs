2e81faccbc81d2243aaa4b9c06e369c29ffec1ab
joaovitorassumpcao <joao.assumpo@gmail.com>
2023-02-20 00:35:38 -0300

    `Rask 3.3`

--- src/ray.rs
+++ src/ray.rs
@@ -1,18 +1,24 @@
+use crate::{
+    vec3,
+    vector::{Color, Direction, Point, Vec3},
+};
 use derive_more::Constructor;
-use crate::{vector::{Direction, Point, Color}, vec3};
 
-#[derive(Debug, Clone, Copy, Constructor)]
+#[derive(Debug, PartialEq, PartialOrd, Clone, Constructor)]
 pub struct Ray {
-    point: Point,
+    origin: Point,
     direction: Direction,
 }
 
 impl Ray {
     pub fn at(&self, t: f64) -> Point {
-		self.point + self.direction * t
-	}
+        self.origin + self.direction * t
+    }
 
     pub fn color(ray: &Ray) -> Color {
-        vec3!(0, 1.0, 0)
+        let direction = ray.direction.normalize();
+        let t = 0.5 * (direction.normalize().y + 1.0);
+
+        Vec3::lerp(vec3!(1.0), vec3!(0.5, 0.7, 1), t)
     }
 }
