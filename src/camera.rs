8dc37f8fa1a5b090a41ed184a32105a5147951fe
joaovitorassumpcao <joao.assumpo@gmail.com>
2023-03-02 00:25:39 -0300

    `impl Default for Camera`

--- src/camera.rs
+++ src/camera.rs
@@ -1,8 +1,33 @@
-use crate::vector::{Point, Vec3};
+use crate::{
+    vec3,
+    vector::{Point, Vec3},
+};
 
-struct Camera {
-	origin: Point,		// Camera a ray origin
-	tl_corner: Point,	// Top Left Corner
-	h_vec: Vec3,		// Viewport's horizontal vector
-	v_vec: Vec3,		// Viewport's vertical vector
+#[derive(Debug, Clone)]
+pub struct Camera {
+    origin: Point,    // Camera a ray origin
+    tl_corner: Point, // Top Left Corner
+    h_vec: Vec3,      // Viewport's horizontal vector
+    v_vec: Vec3,      // Viewport's vertical vector
+}
+
+impl Default for Camera {
+    fn default() -> Self {
+        let aspect_ratio = 16.0 / 9.0;
+		let focal_len = 1.0;
+        let view_height = 2.0;
+        let view_width = aspect_ratio * view_height;
+        let origin: Point = vec3!(0);
+        let h_vec = vec3!(view_width, 0, 0);
+        let v_vec = vec3!(0, -view_height, 0);
+        let tl_corner: Point =
+            origin - h_vec / 2.0 - v_vec / 2.0 - vec3!(0, 0, focal_len);
+
+        Self {
+            origin,
+            tl_corner,
+            h_vec,
+            v_vec,
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -11,47 +11,37 @@ mod vector;
 mod camera;
 
 fn main() {
-    // Set up the image parameters
-    let aspect_ratio = 16.0 / 9.0;
-    let img_width: u32 = 400;
-    let img_height = (img_width as f64 / aspect_ratio) as u32;
+	let camera = camera::Camera::default();
 
-    // Set up the view parameters
-    let view_height = 2.0;
-    let view_width = aspect_ratio * view_height;
-    let focal_len = 1.0;
+	// Set up the image parameters
+	let img_width: u32 = 400;
+	let img_height = (img_width as f64 / aspect_ratio) as u32;
+	
 
-    // Calculate the position of the top left corner of the view
-    let origin: Point = vec3!(0);
-    let horizontal_vec = vec3!(view_width, 0, 0);
-    let vertical_vec = vec3!(0, -view_height, 0);
-    let top_left: Point =
-        origin - horizontal_vec / 2.0 - vertical_vec / 2.0 - vec3!(0, 0, focal_len);
+	let mut img: RgbImage = ImageBuffer::new(img_width, img_height);
 
-    let mut img: RgbImage = ImageBuffer::new(img_width, img_height);
+	let scene: Scene = vec![
+		Box::new(Sphere::new(vec3!(0, 0, -1), 0.5)),
+		Box::new(Sphere::new(vec3!(0, -100.5, -1), 100.0)),
+	];
 
-    let scene: Scene = vec![
-        Box::new(Sphere::new(vec3!(0, 0, -1), 0.5)),
-        Box::new(Sphere::new(vec3!(0, -100.5, -1), 100.0)),
-    ];
+	img.enumerate_pixels_mut().par_bridge().for_each(|(i, j, pixel)| {
+		let samples: u32 = 100;
+		let mut colorpx = vec3!(0);
+		for _ in 0..=samples {
+			// 0.0 <= t <= 1.0
+			let u = (i as f64 + random::<f64>()) / (img_width - 1) as f64;
+			let v = (j as f64 + random::<f64>()) / (img_height - 1) as f64;
 
-    img.enumerate_pixels_mut().par_bridge().for_each(|(i, j, pixel)| {
-        let samples: u32 = 100;
-        let mut colorpx = vec3!(0);
-        for _ in 0..=samples {
-            // 0.0 <= t <= 1.0
-            let u = (i as f64 + random::<f64>()) / (img_width - 1) as f64;
-            let v = (j as f64 + random::<f64>()) / (img_height - 1) as f64;
+			// Calculate the ray direction
+			let ray_dir: Vec3 = top_left + u * horizontal_vec + v * vertical_vec - origin;
+			colorpx = colorpx + ray::color(&Ray::new(origin, ray_dir), &scene);
+		}
 
-            // Calculate the ray direction
-            let ray_dir: Vec3 = top_left + u * horizontal_vec + v * vertical_vec - origin;
-            colorpx = colorpx + ray::color(&Ray::new(origin, ray_dir), &scene);
-        }
+		// Calculate the color for the pixel using the ray
+		*pixel = (colorpx / samples as f64).into();
 
-        // Calculate the color for the pixel using the ray
-        *pixel = (colorpx / samples as f64).into();
+	});
 
-    });
-
-    img.save("render.png").expect("image error");
+	img.save("render.png").expect("image error");
 }
