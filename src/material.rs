6b94360732fbe7c7d74f88f400aeab2bc0432561
joaovitorassumpcao <joao.assumpo@gmail.com>
2023-03-17 15:58:15 -0300

    `raytracer::material::Lambertian::scatter()`

--- src/material.rs
+++ src/material.rs
@@ -1,8 +1,12 @@
-use crate::{object::Hit, ray::Ray, vector::Color};
+use crate::{
+    object::Hit,
+    ray::Ray,
+    vector::{Color, Vec3},
+};
 
 pub struct Reflection {
     ray: Ray,
-	atten_color: Color,
+    atten_color: Color,
 }
 
 pub trait Material {
@@ -12,7 +16,16 @@ pub trait Material {
 pub struct Lambertian(Color);
 
 impl Material for Lambertian {
-    fn scatter(&self, incident_ray: &Ray, hit: &Hit) -> Option<Reflection> {
-        todo!()
+    fn scatter(&self, _incident_ray: &Ray, hit: &Hit) -> Option<Reflection> {
+        let direction = hit.normal + Vec3::rand_unitvec();
+
+        //let origin = hit.intersec;
+        //0.5 * color(&Ray::new(origin, direction), scene, depth - 1)
+
+        let ray = Ray::new(hit.intersec, direction);
+        Some(Reflection {
+            ray,
+            atten_color: self.0,
+        })
     }
 }
